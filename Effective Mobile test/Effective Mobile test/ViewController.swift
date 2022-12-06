1f8dc57db3e42dd7f4a1d2cdb5ca2dc4804dd6e3
mi1212 <mc_gorba4ev@mail.ru>
2022-12-06 16:57:51 +0300

    first commit

---
+++ Effective Mobile test/Effective Mobile test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Effective Mobile test
+//
+//  Created by Mikhail Chuparnov on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
